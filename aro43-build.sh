f43a06365ddbd1a1d674bd3998d38bc91260ee61
haroldwongms <stuart.kirk@microsoft.com>
2020-02-02 04:08:48 -0500

    ARO 4.3 Build Script

--- aro43-build.sh
+++ aro43-build.sh
@@ -46,7 +46,7 @@ echo "Disabling 'PrivateLinkServiceNetworkPolicies' in 'Master' Subnet"
 az network vnet subnet update -g "$RESOURCEGROUP" --vnet-name $VNET_NAME -n "$CLUSTER-master" --disable-private-link-service-network-policies true > /dev/null 2>&1
 
 # Build ARO
-echo "Building Azure Red Hat OpenShift 4.3 - this takes roughly 30 minutes"
+echo "Building Azure Red Hat OpenShift 4.3 - this takes roughly 30 minutes. The time is now: `date`"
 az aro create -g "$RESOURCEGROUP" -n "$CLUSTER" --vnet "$VNET_NAME" --master-subnet "$CLUSTER-master" --worker-subnet "$CLUSTER-worker" --ingress-visibility "$INGRESSPRIVACY" --apiserver-visibility "$APIPRIVACY" --worker-count "$WORKERS" --tags "ARO 4.3 Build Date=`date +%Y%m%d`" 2>/dev/null
 
 # Output Messages
