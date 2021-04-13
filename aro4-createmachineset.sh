169a15000c49d92ae33fe538c0991b843f1b3fa2
Stuart R. Kirk <stuart.kirk@microsoft.com>
2021-04-13 14:43:19 -0400

    Fixed script to work with macos sed

--- aro4-createmachineset.sh
+++ aro4-createmachineset.sh
@@ -28,14 +28,23 @@ CURRENTMACHINESET="$(oc get machineset -n openshift-machine-api -o jsonpath='{.i
 echo "Done."
 
 echo -n "Obtaining required variables..."
+echo -n "azure region, "
 AZUREDC="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.spec.template.spec.providerSpec.value.location}')"
+echo -n "marketplace sku, "
 AROSKU="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.spec.template.spec.providerSpec.value.image.sku}')"
+echo -n "marketplace version, "
 AROSKUVERSION="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.spec.template.spec.providerSpec.value.image.version}')"
+echo -n "cluster name, "
 CLUSTERNAME="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.metadata.labels.machine\.openshift\.io/cluster-api-cluster}')"
+echo -n "network rg, "
 NETWORKRG="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.spec.template.spec.providerSpec.value.networkResourceGroup}')"
+echo -n "cluster rg, "
 PROTECTEDRG="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.spec.template.spec.providerSpec.value.resourceGroup}')"
+echo -n "lb name, "
 PUBLICLBNAME="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.spec.template.spec.providerSpec.value.publicLoadBalancer}')"
+echo -n "worker subnet, "
 SUBNET="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.spec.template.spec.providerSpec.value.subnet}')"
+echo -n "vnet name "
 VNETNAME="$(oc get machineset $CURRENTMACHINESET -n openshift-machine-api -o jsonpath='{.spec.template.spec.providerSpec.value.vnet}')"
 echo "Done."
 
@@ -50,7 +59,7 @@ echo " "
 
 echo -n "Enter the name of the machineset you wish to create (some format of what you see above):  > "
 read MACHINESETNAME
-echo -n "Enter the Azure Availability Zone this machineset should create nodes in (1,2 or 3):  > "
+echo -n "Enter the Azure Availability Zone this machineset should create nodes in (1, 2 or 3):  > "
 read WHICHAZ
 echo -n "Enter the number of worker nodes that should be created for this machineset: > "
 read NUMREPLICAS
@@ -59,8 +68,35 @@ echo -n "Making a copy of the existing machineset template..."
 cp machineset-template.yaml $MACHINESETNAME-template.yaml
 echo "Done."
 
-echo "Dumping avaialble VM SKUs for $AZUREDC:"
-az vm list-sizes -l $AZUREDC -o table
+echo "Current supported Azure Red Hat OpenShift SKUs:"
+echo " "
+echo "GENERAL PURPOSE"
+echo "---------------"
+echo "Series	SKU	                vCPU	Memory: GiB"
+echo "Dasv4	Standard_D4as_v4	4	16"
+echo "Dasv4	Standard_D8as_v4	8	32"
+echo "Dasv4	Standard_D16as_v4	16	64"
+echo "Dasv4	Standard_D32as_v4	32	128"
+echo "Dsv3	Standard_D4s_v3	        4	16"
+echo "Dsv3	Standard_D8s_v3	        8	32"
+echo "Dsv3	Standard_D16s_v3	16	64"
+echo "Dsv3	Standard_D32s_v3	32	128"
+echo " "
+echo "MEMORY OPTIMIZED"
+echo "----------------"
+echo "Series	SKU	                vCPU	Memory: GiB"
+echo "Esv3	Standard_E4s_v3	        4	32"
+echo "Esv3	Standard_E8s_v3	        8	64"
+echo "Esv3	Standard_E16s_v3	16	128"
+echo "Esv3	Standard_E32s_v3	32	256"
+echo " "
+echo "COMPUTE OPTIMIZED"
+echo "-----------------"
+echo "Series	SKU             	vCPU	Memory: GiB"
+echo "Fsv2	Standard_F4s_v2	        4	8"
+echo "Fsv2	Standard_F8s_v2	        8	16"
+echo "Fsv2	Standard_F16s_v2	16	32"
+echo "Fsv2	Standard_F32s_v2	32	64"
 
 echo " "
 echo "#########################################################################################################################################"
@@ -86,21 +122,36 @@ MEMORYMB="$(az vm list-sizes -l $AZUREDC -o tsv |grep $VMSKU | awk '{print $2}')
 VCPUCORES="$(az vm list-sizes -l $AZUREDC -o tsv |grep $VMSKU | awk '{print $4}')"
 
 echo -n "Performing variable substitutions..."
-sed -i "s/MEMORYMB/$MEMORYMB/g" $MACHINESETNAME-template.yaml
-sed -i "s/VCPUCORES/$VCPUCORES/g" $MACHINESETNAME-template.yaml
-sed -i "s/CLUSTERNAME/$CLUSTERNAME/g" $MACHINESETNAME-template.yaml
-sed -i "s/MACHINESETNAME/$MACHINESETNAME/g" $MACHINESETNAME-template.yaml
-sed -i "s/NETWORKRG/$NETWORKRG/g" $MACHINESETNAME-template.yaml
-sed -i "s/PUBLICLBNAME/$PUBLICLBNAME/g" $MACHINESETNAME-template.yaml
-sed -i "s/VNETNAME/$VNETNAME/g" $MACHINESETNAME-template.yaml
-sed -i "s/WHICHAZ/$WHICHAZ/g" $MACHINESETNAME-template.yaml
-sed -i "s/PROTECTEDRG/$PROTECTEDRG/g" $MACHINESETNAME-template.yaml
-sed -i "s/AZUREDC/$AZUREDC/g" $MACHINESETNAME-template.yaml
-sed -i "s/VMSKU/$VMSKU/g" $MACHINESETNAME-template.yaml
-sed -i "s/AROSKUVERSION/$AROSKUVERSION/g" $MACHINESETNAME-template.yaml
-sed -i "s/AROSKU/$AROSKU/g" $MACHINESETNAME-template.yaml
-sed -i "s/SUBNET/$SUBNET/g" $MACHINESETNAME-template.yaml
-sed -i "s/NUMREPLICAS/$NUMREPLICAS/g" $MACHINESETNAME-template.yaml
+echo -n "memory, "
+sed -i'' -e "s/MEMORYMB/$MEMORYMB/g" $MACHINESETNAME-template.yaml
+echo -n "cores, "
+sed -i'' -e "s/VCPUCORES/$VCPUCORES/g" $MACHINESETNAME-template.yaml
+echo -n "cluster name, "
+sed -i'' -e "s/CLUSTERNAME/$CLUSTERNAME/g" $MACHINESETNAME-template.yaml
+echo -n "machineset name, "
+sed -i'' -e "s/MACHINESETNAME/$MACHINESETNAME/g" $MACHINESETNAME-template.yaml
+echo -n "network rg, "
+sed -i'' -e "s/NETWORKRG/$NETWORKRG/g" $MACHINESETNAME-template.yaml
+echo -n "lb name, "
+sed -i'' -e "s/PUBLICLBNAME/$PUBLICLBNAME/g" $MACHINESETNAME-template.yaml
+echo -n "vnet name, "
+sed -i'' -e "s/VNETNAME/$VNETNAME/g" $MACHINESETNAME-template.yaml
+echo -n "availability zone, "
+sed -i'' -e "s/WHICHAZ/$WHICHAZ/g" $MACHINESETNAME-template.yaml
+echo -n "cluster rg, "
+sed -i'' -e "s/PROTECTEDRG/$PROTECTEDRG/g" $MACHINESETNAME-template.yaml
+echo -n "azure region, "
+sed -i'' -e "s/AZUREDC/$AZUREDC/g" $MACHINESETNAME-template.yaml
+echo -n "vm sku, "
+sed -i'' -e "s/VMSKU/$VMSKU/g" $MACHINESETNAME-template.yaml
+echo -n "marketplace version, "
+sed -i'' -e "s/AROSKUVERSION/$AROSKUVERSION/g" $MACHINESETNAME-template.yaml
+echo -n "marketplace sku, "
+sed -i'' -e "s/AROSKU/$AROSKU/g" $MACHINESETNAME-template.yaml
+echo -n "worker subnet, "
+sed -i'' -e "s/SUBNET/$SUBNET/g" $MACHINESETNAME-template.yaml
+echo -n "replicas "
+sed -i'' -e "s/NUMREPLICAS/$NUMREPLICAS/g" $MACHINESETNAME-template.yaml
 echo "Done."
 
 echo "Adding new machineset to Azure Red Hat OpenShift cluster $CLUSTERNAME..."
