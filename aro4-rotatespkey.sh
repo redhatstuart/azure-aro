16dc54eaa1df1dc6318637d7331e56a15f636ca9
Stuart R. Kirk <Stuart.Kirk@microsoft.com>
2022-04-06 15:51:57 -0400

    Update rotate script to use and extend existing SP key

--- aro4-rotatespkey.sh
+++ aro4-rotatespkey.sh
@@ -21,26 +21,34 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-if [ -z "$(az aro list -o table |grep -i  $1)" ]; then
+if [ -z "$(az aro list -o table --only-show-errors |grep -i  $1)" ]; then
     echo "$1 doesn't seem to exist. Review the output of 'az aro list'"
     exit 1
 fi
 
-clusterName="$(az aro list -o table |grep -i $1 | awk '{print $1}')"
-clusterResourceGroup="$(az aro list -o table |grep -i $1 | awk '{print $2}')"
+clusterName="$(az aro list -o table --only-show-errors |grep -i $1 | awk '{print $1}')"
+clusterResourceGroup="$(az aro list -o table --only-show-errors |grep -i $1 | awk '{print $2}')"
+
+echo " "
+echo "Cluster name: $clusterName"
+echo " "
 
 echo -n "Obtaining Azure Service Principal AppID for existing cluster..."
-SPAPPID="$(az aro show -n $clusterName -g $clusterResourceGroup --query servicePrincipalProfile.clientId -o tsv)"
+SPAPPID="$(az aro show -n $clusterName -g $clusterResourceGroup --only-show-errors --query servicePrincipalProfile.clientId -o tsv)"
 echo "Done."
-echo -n "Obtaining Azure Service Principal credential information..."
-CREATED="$(az ad sp credential list --id $SPAPPID --query '[].startDate' -o tsv)"
-EXPIRING="$(az ad sp credential list --id $SPAPPID --query '[].endDate' -o tsv)"
+echo -n "Obtaining Azure Service Principal Secret for texisting cluster..."
+SPSECRET="$(oc get secret azure-credentials -n kube-system -o json | jq -r .data.azure_client_secret | base64 --decode)"
+echo "Done."
+echo -n "Obtaining Azure Service Principal credential create/expiry information..."
+CREATED="$(az ad sp credential list --id $SPAPPID --only-show-errors --query '[].startDate' -o tsv)"
+EXPIRING="$(az ad sp credential list --id $SPAPPID --only-show-errors --query '[].endDate' -o tsv)"
 echo "Done."
 
 echo " "
-echo "Based on your current Azure Red Hat OpenShift AAD Application ID, $SPAPPID, the dates for your current credential are as follows:"
-echo "Create date: $CREATED"
-echo "Expiration date: $EXPIRING"
+echo "Based on your current Azure Red Hat OpenShift Application ID, $SPAPPID, the dates for your current credential are as follows:"
+echo " "
+echo "*   Create date: $CREATED"
+echo "*   Expiration date: $EXPIRING"
 echo " "
 echo "Shall I continue?"
 PS3="Select a numbered option >> "
@@ -53,35 +61,52 @@ case $yn in
 esac
 done
 
+echo " "
 while [[ "$valid" -lt 1 || "$valid" -gt 250 ]]
 do
 
-  echo -n "How many years would you like the new service principal key to be valid for from today's date (1-250) >> "
+  echo -n "How many years would you like the service principal key to be valid for from today's date (1-250) >> "
   read valid
 
+done
+
+echo " "
+echo "Your current Azure Red Hat OpenShift Service Principal secret is:  $SPSECRET"
+echo "Would you like to remove the current secret and generate a new one?"
+PS3="Select a numbered option >> "
+options=("Yes" "No")
+select yn in "${options[@]}"
+do
+case $yn in
+	Yes ) 
+		echo -n "Generating new Azure Service Principal Secret..."
+		SPSECRET="$(cat /proc/sys/kernel/random/uuid | tr -d '\n\r')"
+		echo "Done."
+		break ;;
+	No )
+		echo "Current Azure Service Principal Secret will be used..."
+		break ;;
+esac
 done
 
 expiry="$(date -d "+$valid years" +%Y-%m-%d)"
 echo " "
 
-echo "***********************************************"
-echo "* The new expiration date will be: $expiry *"
-echo "* Sleeping for 10 seconds. CTL-C to abort.    *"
-echo "***********************************************"
+echo "***************************************************"
+echo "* The new key expiration date will be: $expiry *"
+echo "* Sleeping for 10 seconds. CTL-C to abort.        *"
+echo "***************************************************"
 sleep 10
 
 echo " "
 echo -n "Obtaining Azure Service Principal KeyID..."
-SPSECRETKEYID="$(az ad sp credential list --id $SPAPPID -o tsv | awk '{print $4}')"
+SPSECRETKEYID="$(az ad sp credential list --id $SPAPPID -o tsv --only-show-errors | awk '{print $4}')"
 echo "Done."
-echo -n "Generating new Azure Service Principal Secret..."
-NEWSPSECRET="$(cat /proc/sys/kernel/random/uuid | tr -d '\n\r')"
+echo -n "Setting secret $SPSECRET and expiration date $expiry into existing Azure Service Principal..."
+az ad sp credential reset -n $SPAPPID --credential-description "$(date +%m%d%Y%H%M%S)" --end-date "$expiry" -p $SPSECRET --only-show-errors > /dev/null 2>&1
 echo "Done."
-echo -n "Inserting new secret $NEWSPSECRET into existing Azure Service Principal..."
-az ad sp credential reset -n $SPAPPID --credential-description "$(date +%m%d%Y%H%M%S)" --end-date "$expiry" -p $NEWSPSECRET > /dev/null 2>&1
-echo "Done."
-echo "Calling the Azure Linux CLI to push the new secret $NEWSPSECRET into Azure Red Hat OpenShift"
-az aro update -n $clusterName -g $clusterResourceGroup --client-id $SPAPPID --client-secret $NEWSPSECRET
+echo "Calling the Azure Linux CLI to push the changes into Azure Red Hat OpenShift"
+az aro update -n $clusterName -g $clusterResourceGroup --client-id $SPAPPID --client-secret $SPSECRET --only-show-errors
 
 echo " "
 echo "Please remember that if you are using the same service principal to connect to Azure Active Directory you will need to"
